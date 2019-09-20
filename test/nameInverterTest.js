2bc00fec6b34c461bfaa59b021db3d08bfe834ba
Tyler <t.mar92@gmail.com>
2019-09-20 20:59:13 +0000

    wrote unit tests - not attempted stretch cases

--- test/nameInverterTest.js
+++ test/nameInverterTest.js
@@ -5,4 +5,24 @@ const nameInverter = require('../nameInverter');
 
 describe('nameInverter', function() {
 
+  it("should return an empty string when passed an empty string", function() {
+    const answer = nameInverter("");
+    assert.equal(answer, "", "yay");
+  });
+
+  it("should return a single name when passed a single name", function() {
+    const answer = nameInverter("john");
+    assert.equal(answer, "john", "yay");
+    console.log(answer);
+  });
+
+  it("should return a single name when passed a single name with extra spaces", function() {
+    const answer = nameInverter("  john ");
+    assert.equal(answer, "john", "yay");
+  });
+
+  it("should return last, first when passed both names", function() {
+    const answer = nameInverter("john gregorinko");
+    assert.equal(answer, "gregorinko, john", "yay");
+  });
 });
